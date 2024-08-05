9972c9d4a34b804e5449e6b007ff9c152a1c1a86
Binh Do <binhdothanh.navn@gmail.com>
2024-08-05 13:40:52 +0700

    Update camera_awesome_builder.dart

--- lib/src/widgets/camera_awesome_builder.dart
+++ lib/src/widgets/camera_awesome_builder.dart
@@ -363,7 +363,7 @@ class _CameraWidgetBuilder extends State<CameraAwesomeBuilder>
 
   @override
   void didChangeDependencies() {
-    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+    // SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     super.didChangeDependencies();
   }
 
